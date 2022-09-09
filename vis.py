ad7c3ddbcb78eb9cdf86690005e530bf91402d29
Oliver Balfour <oliver.leo.balfour@gmail.com>
2022-09-09 00:37:03 -0700

    proper input mechanism

--- vis.py
+++ vis.py
@@ -1,5 +1,6 @@
 import random
 import re
+import json
 
 def unique_custom_elements(html: str) -> str:
   """HTML custom elements can't be re-registered/redefined so when developing visualisations
@@ -15,21 +16,31 @@ _tag = r'<\s*/?\s*[a-z][a-z0-9\._-]*-[a-z0-9\._-]*'
 _reg = r'customElements.define\(["\'][^"\'\n]+'
 _regex = re.compile(rf'({_tag}|{_reg})')
 
-def vis(fname: str):
+def script_for_variables(**kwargs):
+  "Creates a <script> tag containing all the kwargs as JS global variables"
+
+  # convert Python objects into things that can be serialized to JS objects
+  # string matching the type is hacky (subclasses won't work) but it avoids
+  # conditionally importing lots of tensor libraries
+  def proc(obj):
+    ty = str(type(obj))
+    if 'ndarray' in ty: return obj.tolist()
+    elif 'torch.Tensor' in ty or 'torch.nn.Parameter' in ty: return obj.tolist()
+    else: return obj
+  serializable = { name: proc(kwargs[name]) for name in kwargs }
+
+  # concatenate and return
+  script = "<script>\nconsole.clear()\nwindow.latest_parameters = {\n"
+  for var_name in serializable:
+    script += f"  {var_name}: {json.dumps(serializable[var_name])},\n"
+  script += "}\n</script>\n"
+  return script
+
+def vis(fname: str, **js_global_variable_kwargs):
   "This returns the visualisation object which can be displayed by IPython"
   from IPython.display import HTML
+  html = script_for_variables(**js_global_variable_kwargs)
   with open(fname, 'r') as f:
-    html = f.read()
+    html += f.read()
   html = unique_custom_elements(html)
   return HTML(html)
-
-
-"""
-Other desiderata for injection:
-* A thing that renders error messages if something fails instead of showing nothing
-* A thing that injects relative imports, unless we can get imports to just work
-   Relative imports with `import ... from './file.js'` seem to work, but do
-   they get updated how you'd expect, or interfere with previous widgets? If so,
-   injecting strings is the way to go (for relative imports ONLY)
-* console.clear at the start of each new widget?
-"""
