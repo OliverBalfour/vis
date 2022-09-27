f03aaf350f3a11f5bd4ab42eb1c17894da2eb908
Oliver Balfour <oliver.leo.balfour@gmail.com>
2022-09-27 17:57:17 +0200

    add open in browser option

--- vis.py
+++ vis.py
@@ -1,20 +1,23 @@
 import random
 import re
 import json
+from IPython.display import display, HTML
 
-def unique_custom_elements(html: str) -> str:
+def make_names_unique(html: str) -> str:
   """HTML custom elements can't be re-registered/redefined so when developing visualisations
   you need to reload the whole VSCode window / Jupyter tab. As a workaround, we inject random
-  numbers into the HTML string at the end of every element name"""
+  numbers into the HTML string at the end of every element name.
+  We also make the latest parameter mounting point unique."""
   i = random.getrandbits(24)
   # Scan for <custom-elem or </custom-elem or customElements.define('custom-elem with regex
-  return _regex.sub(rf'\1-{i}', html)
+  return _regex.sub(rf'\1_{i}', html)
 
 # Regex for custom element tags and definitions, should hit most cases
 # https://html.spec.whatwg.org/multipage/custom-elements.html#valid-custom-element-name
-_tag = r'<\s*/?\s*[a-z][a-z0-9\._-]*-[a-z0-9\._-]*'
-_reg = r'customElements.define\(["\'][^"\'\n]+'
-_regex = re.compile(rf'({_tag}|{_reg})')
+_custom_element_tag = r'<\s*/?\s*[a-z][a-z0-9\._-]*-[a-z0-9\._-]*'
+_custom_element_def = r'customElements.define\(["\'][^"\'\n]+'
+_latest_parameters = r'window\.latest_parameters'
+_regex = re.compile(rf'({_custom_element_tag}|{_custom_element_def}|{_latest_parameters})')
 
 def script_for_variables(**kwargs):
   "Creates a <script> tag containing all the kwargs as JS global variables"
@@ -36,11 +39,18 @@ def script_for_variables(**kwargs):
   script += "}\n</script>\n"
   return script
 
+class WrappedHTML(HTML):
+  def browser(self):
+    import webbrowser as wb
+    import tempfile
+    with tempfile.NamedTemporaryFile(mode='w', suffix='.html', delete=False) as f:
+      f.write(self.data)
+      wb.open('file://' + f.name, new=2)
+
 def vis(fname: str, **js_global_variable_kwargs):
   "This returns the visualisation object which can be displayed by IPython"
-  from IPython.display import HTML
   html = script_for_variables(**js_global_variable_kwargs)
   with open(fname, 'r') as f:
     html += f.read()
-  html = unique_custom_elements(html)
-  return HTML(html)
+  html = make_names_unique(html)
+  return WrappedHTML(html)
